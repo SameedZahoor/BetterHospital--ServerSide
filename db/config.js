ef57f080e2902a7b16cd11c5afbe68bd1a205581
Sameed Zahoor <84683208+akasami@users.noreply.github.com>
2022-06-02 18:29:15 +0500

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-'mongodb+srv://sameedzahoor:UDH4qhHore2VPMZq@cluster0.sqiv4.mongodb.net/BetterHospital?retryWrites=true&w=majority',
-//'mongodb+srv://usamafiaz:usama2001fiaz@cluster0.u1gsm.mongodb.net/Usama?retryWrites=true&w=majority',
+'mongodb atlas key here.',
 {
     useNewUrlParser:true,
     useUnifiedTopology:true
